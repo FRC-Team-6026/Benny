251b38a9fc4492801faf4cec22129e88cdb8ec20
TJ Rockefeller <tjrockefeller@users.noreply.github.com>
2019-02-11 23:40:41 -0600

    wpilibj.CameraServer is deprecated.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,7 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
